6bf03da12936b503f0d612a98b556b01b2efe153
JoseLuisUgaldeM <jlugalde227z@fpdrioja.com>
2026-01-14 16:43:33 +0100

    v2.1

--- sql/bdtruechange.sql
+++ sql/bdtruechange.sql
@@ -1,25 +1,10 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.32-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.11.0.7065
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para bdtruechange
-CREATE DATABASE IF NOT EXISTS `bdtruechange` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+CREATE DATABASE IF NOT EXISTS `bdtruechange` 
 USE `bdtruechange`;
 
--- Volcando estructura para tabla bdtruechange.articulos
+
 CREATE TABLE IF NOT EXISTS `articulos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_id` int(11) NOT NULL,
@@ -28,41 +13,38 @@ CREATE TABLE IF NOT EXISTS `articulos` (
   `categoria` varchar(100) DEFAULT NULL,
   `estado` enum('nuevo','como nuevo','usado','deteriorado') DEFAULT 'usado',
   `fecha_publicacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estadoArticulo` enum('disponible','reservado','vendido') DEFAULT 'disponible',
+  `comprador_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`),
-  CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `fk_comprador` (`comprador_id`),
+  CONSTRAINT `articulos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_comprador` FOREIGN KEY (`comprador_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.articulos_fotos
 CREATE TABLE IF NOT EXISTS `articulos_fotos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `articulo_id` int(11) NOT NULL,
   `ruta_foto` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `articulo_id` (`articulo_id`),
-  CONSTRAINT `articulos_fotos_ibfk_1` FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `articulos_fotos_ibfk_1` (`articulo_id`),
+  CONSTRAINT `articulos_fotos_ibfk_1` FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`) ON DELETE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.chats
-CREATE TABLE IF NOT EXISTS `chats` (
+CREATE TABLE IF NOT EXISTS `favoritos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario1_id` int(11) NOT NULL,
-  `usuario2_id` int(11) NOT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_usuario` int(11) NOT NULL,
+  `id_articulo` int(11) NOT NULL,
+  `fecha_guardado` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `usuario1_id` (`usuario1_id`),
-  KEY `usuario2_id` (`usuario2_id`),
-  CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`usuario1_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `chats_ibfk_2` FOREIGN KEY (`usuario2_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `id_usuario` (`id_usuario`,`id_articulo`),
+  KEY `id_articulo` (`id_articulo`),
+  CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`id_articulo`) REFERENCES `articulos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
-
--- Volcando estructura para tabla bdtruechange.intercambios
 CREATE TABLE IF NOT EXISTS `intercambios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `solicitante_id` int(11) NOT NULL,
@@ -82,25 +64,20 @@ CREATE TABLE IF NOT EXISTS `intercambios` (
   CONSTRAINT `intercambios_ibfk_4` FOREIGN KEY (`articulo_receptor`) REFERENCES `articulos` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.mensajes
-CREATE TABLE IF NOT EXISTS `mensajes` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `chat_id` int(11) NOT NULL,
-  `emisor_id` int(11) NOT NULL,
-  `mensaje` text NOT NULL,
-  `fecha_envio` timestamp NOT NULL DEFAULT current_timestamp(),
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message_text` text NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  `is_read` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`),
-  KEY `chat_id` (`chat_id`),
-  KEY `emisor_id` (`emisor_id`),
-  CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `mensajes_ibfk_2` FOREIGN KEY (`emisor_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `sender_id` (`sender_id`),
+  KEY `receiver_id` (`receiver_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=262 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.notificaciones
 CREATE TABLE IF NOT EXISTS `notificaciones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_id` int(11) NOT NULL,
@@ -113,44 +90,37 @@ CREATE TABLE IF NOT EXISTS `notificaciones` (
   CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.resenas
-CREATE TABLE IF NOT EXISTS `resenas` (
+CREATE TABLE IF NOT EXISTS `reseñas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `evaluador_id` int(11) NOT NULL,
+  `articulo_id` int(11) DEFAULT NULL,
+  `emisor_id` int(11) DEFAULT NULL,
+  `receptor_id` int(11) DEFAULT NULL,
+  `puntuacion` int(11) DEFAULT NULL CHECK (`puntuacion` >= 1 and `puntuacion` <= 5),
   `comentario` text DEFAULT NULL,
-  `puntuacion` int(11) DEFAULT NULL CHECK (`puntuacion` between 1 and 5),
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
+  `fecha` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `usuario_id` (`usuario_id`),
-  KEY `evaluador_id` (`evaluador_id`),
-  CONSTRAINT `resenas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `resenas_ibfk_2` FOREIGN KEY (`evaluador_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `articulo_id` (`articulo_id`),
+  KEY `emisor_id` (`emisor_id`),
+  KEY `receptor_id` (`receptor_id`),
+  CONSTRAINT `reseñas_ibfk_1` FOREIGN KEY (`articulo_id`) REFERENCES `articulos` (`id`),
+  CONSTRAINT `reseñas_ibfk_2` FOREIGN KEY (`emisor_id`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `reseñas_ibfk_3` FOREIGN KEY (`receptor_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla bdtruechange.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
-  `apellido1` varchar(150) DEFAULT NULL,
-  `apellido2` varchar(150) DEFAULT NULL,
+  `apellido1` varchar(150) NOT NULL,
+  `apellido2` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) NOT NULL DEFAULT 'imagenes/uploads/default.png',
+  `ciudad` varchar(255) DEFAULT NULL,
   `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp(),
-  `localidad` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
